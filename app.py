7d9372b7a7c695911d9cc6406b273a49207ca937
rmg <rmgajdosik@gmail.com>
2025-07-11 11:17:56 +0200

    Created models folder for ORM , updated app.py accordingly

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, render_template,redirect
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
