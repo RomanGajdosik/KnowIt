d0ca525870418e58963249a87cc7a2103a6091db
rmg <rmgajdosik@gmail.com>
2025-07-13 12:07:11 +0200

    renamed, added  properties for login

---
+++ models/usersDB.py
@@ -0,0 +1,26 @@
+from app import db
+
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password_hash = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    role = db.Column(db.String(50), default='student')  # Default role is 'user'
+
+    # Flask-Login required methods
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.id)
